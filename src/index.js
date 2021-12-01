e1fc6d5960fb7b963fcd3cfb058724709cb4d51f
gregaswright <greg.a.s.wright@gmail.com>
2021-12-01 11:36:28 -0800

    second commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const port = process.env.PORT || 3000;
 
@@ -28,12 +29,30 @@ io.on('connection', (socket) => {
     console.log('New Web Socket')
 
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room)
-       
-    })
+        const { error, user } = addUser({ id: socket.id, name, room});
 
-    socket.on('disconnect', () => {
+        if(error) return callback(error);
+
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined`})
+
+        socket.join(user.room);
+       
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message})
+
+        callback();
+    });
+
+    socket.on('disconnect', ({name}) => {
         console.log('User has left')
+        io.emit('message', {user: 'admin', text:`${user.name} has left!`})
     })
 })
 
